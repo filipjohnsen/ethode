717e5f34ec54f403ca76bf2d6ec5113d08fdc700
filipjohnsen <filipjohnsen97@gmail.com>
2017-08-30 15:00:50 +0200

    Add files via upload

---
+++ ethode/header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="css/style.css">
+        <title>ethode - webdesign mm.</title>
+        <script src="jquery-3.2.1.min.js"></script>
+    </head>
+    <body>
+        <header>
+            <nav>
+                <div class="logo"><img src="img/logo.svg"></div>    
+                <ul>
+                    <li><a href="#">mitt arbeid</a></li>
+                    <li><a href="#">kontakt meg</a></li>
+                    <li><a href="#"><input placeholder="hva skal stå?"></a></li>
+                </ul>
+            </nav>
+        </header>
---
+++ ethode/index.php
@@ -0,0 +1,13 @@
+<?php include 'header.php'?>
+    <div id="about_me">
+        <div class="right">
+            <div class="inner_right">
+
+            </div>
+        </div>
+    </div>
+<div id="contact_page">
+    <hr>
+    <h1>Portfolio</h1>
+</div>
+<?php include 'footer.php'?>
